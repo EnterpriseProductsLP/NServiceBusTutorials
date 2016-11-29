4f0a563b2b622d2dc091dc08fdfbeca4cbe9b936
Eric Burcham <eburcham@eprod.com>
2016-11-29 14:20:50 -0600

    Added project files.

---
+++ Source/PublishSubscribe/NServiceBusTutorials.PublishSubscribe.SubscriberOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBusTutorials.PublishSubscribe.SubscriberOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
