a1d05a4a69724e9429c107f49f1de1bb534d480e
Eric Burcham <eburcham@eprod.com>
2018-10-30 10:11:20 -0500

    Updated docker script to use create instead of run.

--- Source/install-rabbit-container.ps1
+++ Source/install-rabbit-container.ps1
@@ -1,1 +1,1 @@
-docker run -d --name="rabbit1" --hostname="rabbit1" -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+docker create --name="rabbit1" --hostname="rabbit1" -p 5672:5672 -p 15672:15672 rabbitmq:3-management
